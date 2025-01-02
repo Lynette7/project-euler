628a4e07aefcb05bc8e6c1e04fe4c7487397877d
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2025-01-02 23:27:01 +0300

    chore: pandigital multiples problem 38

--- p41-pandigital-prime/src/main.rs
+++ p41-pandigital-prime/src/main.rs
@@ -6,12 +6,12 @@ fn is_prime(n: u64) -> bool {
         return true;
     }
     if n % 2 == 0 || n % 3 == 0 {
-        return  false;;
+        return false;
     }
     let mut i = 5;
     while i * i <= n {
         if n % i == 0 || n % (i + 2) == 0 {
-            return  false;;
+            return false;
         }
         i += 6;
     }
