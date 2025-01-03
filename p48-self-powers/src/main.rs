3d9ecff4142a7bb79262160ac297c1d25bfec016
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2025-01-03 22:28:16 +0300

    chore: self primes

--- p48-self-powers/src/main.rs
+++ p48-self-powers/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    const MOD: u64 = 10_000_000_000;
+    let mut sum: u64 = 0;
+
+    for i in 1..=1000 {
+        let mut power = 1u64;
+        for _ in 0..i {
+            power = power * i % MOD;
+        }
+        sum = (sum + power) % MOD;
+    }
+
+    println!("The last ten digits of the series are: {:010}", sum);
 }
